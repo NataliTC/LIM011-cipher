c05c1881a91884352eb6f6fb267f05b470868688
NataliTC <natali.tauma@gmail.com>
2019-10-11 10:23:28 -0500

    segundo avance TEST1

--- src/index.js
+++ src/index.js
@@ -1,60 +1,39 @@
-// import { Cipher } from "crypto";
+const boton = document.getElementById('boton');
+const cifrado = document.getElementById('cifrado');
+const descifrado = document.getElementById('descifrado');
+const inputMessage = document.getElementById('inputMessage');
+const inputNumber = document.getElementById('inputNumber');
+// eslint-disable-next-line no-unused-vars
+const pintame = document.getElementById('pintame');
+const nuevo = document.getElementById('nuevo');
+const salir = document.getElementById('salir');
 
-const boton=document.getElementById('boton');
-
-const cifrado=document.getElementById('cifrado');
-const descifrado=document.getElementById('descifrado');
-
-const inputMessage=document.getElementById('inputMessage');
-  const inputNumber = document.getElementById('inputNumber');
-
-const pintame=document.getElementById('pintame');
-
-
-const nuevo=document.getElementById('nuevo');
-const salir=document.getElementById('salir');
-
-
-boton.addEventListener('click',()=>{
+boton.addEventListener('click', () => {
   root.classList.add('hide');
   message.classList.remove('hide');
-}
-);
-
-
-
-cifrado.addEventListener('click',()=>{
-  message.classList.add('hide');
-  resultado.classList.remove('hide');
-  
-  const resultado1 = cipher.encode(inputNumber.value, inputMessage.value);
-  document.getElementById('pintame').innerHTML = resultado1;
-
-  
 });
 
+cifrado.addEventListener('click', () => {
+  message.classList.add('hide');
+  resultado.classList.remove('hide');
+  const resultado1 = cipher.encode(inputNumber.value, inputMessage.value);
+  document.getElementById('pintame').innerHTML = resultado1;
+});
 
-descifrado.addEventListener('click',()=>{
+descifrado.addEventListener('click', () => {
   message.classList.add('hide');
   resultado.classList.remove('hide');
 
   const resultado2 = cipher.decode(inputNumber.value, inputMessage.value);
   document.getElementById('pintame').innerHTML = resultado2;
-}
-);
+});
 
-
-nuevo.addEventListener('click',()=>{
+nuevo.addEventListener('click', () => {
   resultado.classList.add('hide');
   message.classList.remove('hide');
+});
 
-}
-);
-
-
-salir.addEventListener('click',()=>{
+salir.addEventListener('click', () => {
   resultado.classList.add('hide');
   root.classList.remove('hide');
-
-}
-);
+});
